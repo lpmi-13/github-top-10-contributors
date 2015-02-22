3ef54b1caced22d336d8f2e5e1752e4f2b7003eb
Calvin Mai <calvin.mai@gmail.com>
2015-02-22 01:31:24 -0800

    added unit tests

---
+++ unit_tests.js
@@ -0,0 +1,30 @@
+var github = require("./github_facade");
+
+// ----- getUserDetailByUserName -----
+exports.getTopContributorsByUserAndRepos = {
+	TenContributorsReturned: function(test){
+		github.getTopContributorsByUserAndRepos('sage', 'streamlinejs', 10, function(user, repos, statusCode, json){
+			test.equal(json.length, 10, 'Should return 10 contributors from GitHub for this repository.');
+			
+			test.done();
+		});
+	}
+};
+
+// ----- getUserDetailByUserName -----
+exports.getUserDetailByUserName = {
+	NonExistingUserTest: function(test){
+		github.getUserDetailByUserName('non-existing', function(statusCode, json){
+			test.equal(statusCode, 404, 'This user should not exist in GitHub and 404 status code should be returned.');
+			
+			test.done();
+		});
+	},
+	ExistingUserTest: function(test){
+		github.getUserDetailByUserName('calvin-m', function(statusCode, json){
+			test.equal(statusCode, 200, 'This user should exist in GitHub and 200 status code should be returned.');
+			
+			test.done();
+		});
+	}
+};
